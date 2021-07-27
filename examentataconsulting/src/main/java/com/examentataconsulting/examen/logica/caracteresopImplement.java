810e369c8fa645010d9f4af39588ec15e818c331
Lenovo <anderson6466@hotmail.com>
2021-07-27 10:00:58 -0500

    act

--- examentataconsulting/src/main/java/com/examentataconsulting/examen/logica/caracteresopImplement.java
+++ examentataconsulting/src/main/java/com/examentataconsulting/examen/logica/caracteresopImplement.java
@@ -67,7 +67,68 @@ public class caracteresopImplement  implements caracterInterface {
 	@Override
 	public boolean ingresarCaracter2(requestdto Requestdto) {
 		
-		return false;
+		
+		
+		boolean resultfinal=true;
+		
+     	int  count=0;
+     	int contador2palabras=0;
+     	boolean result=true;
+	
+	
+     	String cadena = Requestdto.getCaracter();
+     	for (int i = 0; i < cadena.length(); i++){
+     	    char letra1 = cadena.charAt(i);
+     	 	if(count!=contador2palabras) {
+     	  		
+     	 		
+     	 		if(contador2palabras != count+1     )
+	     	  	  resultfinal=false;
+	     	  		break;
+	     	 
+     	 	
+     	 	
+     	 	}
+     	   contador2palabras=0;
+     	  	for (int j = 0; j < cadena.length(); j++){
+	     	   
+     	  		
+     	  		char letra2 = cadena.charAt(j);
+	     	    if(  letra2==letra1) {
+	     	    contador2palabras=contador2palabras+1;
+	     	    
+	     	   
+		     	     if(result) {
+	     	    	
+	     	    	count=count+1;
+	     	    	
+	     	    	}
+	     	    	
+	     	    }
+	     	    else if(letra2!=letra1) {
+	     	   	result=false;
+	     	    	 continue;
+	     	    	
+	     	    	
+	     	    }
+	     	    
+	     	    
+	     	    
+	     	    
+	     	 
+	     	}
+	     	
+     	 
+     	  	
+     	  
+     	}
+     	
+     	System.out.print(resultfinal);
+return resultfinal;
+		
+		
+		
+	 
 	}
 
  
